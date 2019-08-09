6fb280da97c3bc62dce90fbd87cbcaecb48cfd2c
martin-banks <anotherbanksy@gmail.com>
2019-08-09 13:02:08 +1000

    remove stock content from landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>DT</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Welcome to the DT Digital Toolkit</p>
+
   </Layout>
 )
 
