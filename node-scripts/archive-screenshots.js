c19bd42931ce46b4b72fe7aeb28fdd2711a15186
martin-banks <anotherbanksy@gmail.com>
2019-08-10 03:13:27 +1000

    adds function to save screenshots of archive projects

---
+++ node-scripts/archive-screenshots.js
@@ -0,0 +1,67 @@
+const pup = require('puppeteer')
+const frontMatter = require('front-matter')
+const fs = require('fs')
+const path = require('path')
+// const importAllImages = require('./src/helpers/import-all-images')
+
+const blacklist = [
+  '.DS_Store',
+]
+
+async function wait () {
+  return new Promise((resolve, reject) => {
+    console.log('starting wait')
+    setTimeout(() => {
+      console.log('still waiting...')
+      resolve()
+    }, 5000)
+  })
+}
+
+;(async () => {
+  const mdFiles = fs.readdirSync(
+    path.join(__dirname, '../src/dt-archive'),
+    (err, files) => {
+      if (err) return console.log('Error reading directory ->\n', err)
+    }
+  )
+    .filter(f => blacklist.indexOf(f))
+
+  for (file of mdFiles) {
+    console.log({ file })
+    const browser = await pup.launch()
+    try {
+      const fileContent = fs.readFileSync(
+        path.join(__dirname, `../src/dt-archive/${file}`),
+        (err, content) => {
+          if (err) throw err
+        }
+      )
+      const previewUrl = frontMatter(fileContent.toString()).attributes.preview
+      const title = frontMatter(fileContent.toString())
+       .attributes
+       .title
+       .toLowerCase()
+       .replace(/\s+/g, '-')
+      const page = await browser.newPage()
+      page.setViewport({
+        width: 650,
+        height: 940,
+        deviceScaleFactor: 1,
+      })
+      await page.goto(
+        previewUrl,
+        { waitUntil: 'networkidle2' }
+      )
+
+      await wait()
+      console.log('waited... ', title)
+      await page.screenshot({ path: `../src/screengrabs/${title}.png`})
+      await browser.close()
+    } catch (err) {
+      console.log({ err })
+    }
+  }
+})()
+
+
